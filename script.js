811660e293ae4c165de64fe6abdd789ae2272039
jesseQuayle10 <jessequayle10@gmail.com>
2023-05-28 14:27:53 +1200

    Latest Updates

--- script.js
+++ script.js
@@ -15,6 +15,22 @@ const cardThreeImg = document.querySelector('#card-3-img');
 const cardFourImg = document.querySelector('#card-4-img');
 const cardFiveImg = document.querySelector('#card-5-img');
 
+// BTNs
+const dealBtn = document.querySelector('#deal-btn');
+
+const holdBtnOne = document.querySelector('#card-1-btn');
+const holdBtnTwo = document.querySelector('#card-2-btn');
+const holdBtnThree = document.querySelector('#card-3-btn');
+const holdBtnFour = document.querySelector('#card-4-btn');
+const holdBtnFive = document.querySelector('#card-5-btn');
+
+//// Gl. VARs
+let cardOneIsHeld = false;
+let cardTwoIsHeld = false;
+let cardThreeIsHeld = false;
+let cardFourIsHeld = false;
+let cardFiveIsHeld = false;
+
 //// Gen. Suit & Num
 const generateSuit = function () {
   return Math.floor(Math.random() * 4 + 1);
@@ -117,107 +133,198 @@ const numberSwitch = function (number) {
 // Upd the .src in DOM
 
 const cardResult1 = function () {
-  let cardSuit = 0;
-  let cardNumber = 0;
+  // Only Run IF Card isnt "Held"
+  if (!cardOneIsHeld) {
+    // To Hold Returns from Gen. Random Nums
+    let cardSuit = 0;
+    let cardNumber = 0;
 
-  // Call FN & Grab its Gen. Suit & Num
-  cardSuit = generateSuit();
-  cardNumber = generateNumber();
+    // Call FN & Grab its Gen. Suit & Num
+    cardSuit = generateSuit();
+    cardNumber = generateNumber();
 
-  // Call FNs w/ Switches in them - Passing the Rand. Nums
-  // Grab the Returned Suit & Card Num from them
-  const suitResult = suitSwitch(cardSuit);
-  const numberResult = numberSwitch(cardNumber);
+    // Call FNs w/ Switches in them - Passing the Rand. Nums
+    // Grab the Returned Suit & Card Num from them
+    const suitResult = suitSwitch(cardSuit);
+    const numberResult = numberSwitch(cardNumber);
 
-  // File name STR (to match IMG File name - for Rendering)
-  let cardName = `./images/${suitResult}_${numberResult}.svg`;
+    // File name STR (to match IMG File name - for Rendering)
+    let cardName = `./images/${suitResult}_${numberResult}.svg`;
 
-  // Upd the SRC for IMG
-  cardOneImg.src = `${cardName}`;
+    // Upd the SRC for IMG
+    cardOneImg.src = `${cardName}`;
+  }
 };
-
-cardResult1();
 
 const cardResult2 = function () {
-  let cardSuit = 0;
-  let cardNumber = 0;
-  // Call FN & Grab its Gen. Suit & Num
-  cardSuit = generateSuit();
-  cardNumber = generateNumber();
+  // Only Run IF Card isnt "Held"
+  if (!cardTwoIsHeld) {
+    // To Hold Returns from Gen. Random Nums
+    let cardSuit = 0;
+    let cardNumber = 0;
+    // Call FN & Grab its Gen. Suit & Num
+    cardSuit = generateSuit();
+    cardNumber = generateNumber();
 
-  // Call FNs w/ Switches in them - Passing the Rand. Nums
-  // Grab the Returned Suit & Card Num from them
-  const suitResult = suitSwitch(cardSuit);
-  const numberResult = numberSwitch(cardNumber);
+    // Call FNs w/ Switches in them - Passing the Rand. Nums
+    // Grab the Returned Suit & Card Num from them
+    const suitResult = suitSwitch(cardSuit);
+    const numberResult = numberSwitch(cardNumber);
 
-  // File name STR (to match IMG File name - for Rendering)
-  let cardName = `./images/${suitResult}_${numberResult}.svg`;
+    // File name STR (to match IMG File name - for Rendering)
+    let cardName = `./images/${suitResult}_${numberResult}.svg`;
 
-  // Upd the SRC for IMG
-  cardTwoImg.src = `${cardName}`;
+    // Upd the SRC for IMG
+    cardTwoImg.src = `${cardName}`;
+  }
 };
-
-cardResult2();
 
 const cardResult3 = function () {
-  let cardSuit = 0;
-  let cardNumber = 0;
-  // Call FN & Grab its Gen. Suit & Num
-  cardSuit = generateSuit();
-  cardNumber = generateNumber();
+  // Only Run IF Card isnt "Held"
+  if (!cardThreeIsHeld) {
+    // To Hold Returns from Gen. Random Nums
+    let cardSuit = 0;
+    let cardNumber = 0;
+    // Call FN & Grab its Gen. Suit & Num
+    cardSuit = generateSuit();
+    cardNumber = generateNumber();
 
-  // Call FNs w/ Switches in them - Passing the Rand. Nums
-  // Grab the Returned Suit & Card Num from them
-  const suitResult = suitSwitch(cardSuit);
-  const numberResult = numberSwitch(cardNumber);
+    // Call FNs w/ Switches in them - Passing the Rand. Nums
+    // Grab the Returned Suit & Card Num from them
+    const suitResult = suitSwitch(cardSuit);
+    const numberResult = numberSwitch(cardNumber);
 
-  // File name STR (to match IMG File name - for Rendering)
-  let cardName = `./images/${suitResult}_${numberResult}.svg`;
+    // File name STR (to match IMG File name - for Rendering)
+    let cardName = `./images/${suitResult}_${numberResult}.svg`;
 
-  // Upd the SRC for IMG
-  cardThreeImg.src = `${cardName}`;
+    // Upd the SRC for IMG
+    cardThreeImg.src = `${cardName}`;
+  }
 };
-
-cardResult3();
 
 const cardResult4 = function () {
-  let cardSuit = 0;
-  let cardNumber = 0;
-  // Call FN & Grab its Gen. Suit & Num
-  cardSuit = generateSuit();
-  cardNumber = generateNumber();
+  // Only Run IF Card isnt "Held"
+  if (!cardFourIsHeld) {
+    // To Hold Returns from Gen. Random Nums
+    let cardSuit = 0;
+    let cardNumber = 0;
+    // Call FN & Grab its Gen. Suit & Num
+    cardSuit = generateSuit();
+    cardNumber = generateNumber();
 
-  // Call FNs w/ Switches in them - Passing the Rand. Nums
-  // Grab the Returned Suit & Card Num from them
-  const suitResult = suitSwitch(cardSuit);
-  const numberResult = numberSwitch(cardNumber);
+    // Call FNs w/ Switches in them - Passing the Rand. Nums
+    // Grab the Returned Suit & Card Num from them
+    const suitResult = suitSwitch(cardSuit);
+    const numberResult = numberSwitch(cardNumber);
 
-  // File name STR (to match IMG File name - for Rendering)
-  let cardName = `./images/${suitResult}_${numberResult}.svg`;
+    // File name STR (to match IMG File name - for Rendering)
+    let cardName = `./images/${suitResult}_${numberResult}.svg`;
 
-  // Upd the SRC for IMG
-  cardFourImg.src = `${cardName}`;
+    // Upd the SRC for IMG
+    cardFourImg.src = `${cardName}`;
+  }
 };
-
-cardResult4();
 
 const cardResult5 = function () {
-  let cardSuit = 0;
-  let cardNumber = 0;
-  // Call FN & Grab its Gen. Suit & Num
-  cardSuit = generateSuit();
-  cardNumber = generateNumber();
+  // Only Run IF Card isnt "Held"
+  if (!cardFiveIsHeld) {
+    // To Hold Returns from Gen. Random Nums
+    let cardSuit = 0;
+    let cardNumber = 0;
+    // Call FN & Grab its Gen. Suit & Num
+    cardSuit = generateSuit();
+    cardNumber = generateNumber();
 
-  // Call FNs w/ Switches in them - Passing the Rand. Nums
-  // Grab the Returned Suit & Card Num from them
-  const suitResult = suitSwitch(cardSuit);
-  const numberResult = numberSwitch(cardNumber);
+    // Call FNs w/ Switches in them - Passing the Rand. Nums
+    // Grab the Returned Suit & Card Num from them
+    const suitResult = suitSwitch(cardSuit);
+    const numberResult = numberSwitch(cardNumber);
 
-  // File name STR (to match IMG File name - for Rendering)
-  let cardName = `./images/${suitResult}_${numberResult}.svg`;
+    // File name STR (to match IMG File name - for Rendering)
+    let cardName = `./images/${suitResult}_${numberResult}.svg`;
 
-  // Upd the SRC for IMG
-  cardFiveImg.src = `${cardName}`;
+    // Upd the SRC for IMG
+    cardFiveImg.src = `${cardName}`;
+  }
 };
 
-cardResult5();
+//// EVTs
+// Deal/Draw BTN
+
+let dealCount = 0;
+
+dealBtn.addEventListener('click', function (e) {
+  // VAR so no More than 2 Deal/Draws Poss
+  dealCount++;
+  console.log(dealCount);
+
+  // Call Gen. Rand Nums FNs
+  // bc ea "card FN" has eg. "cardFiveIsHeld" - these will ONLY Exec. if !cardFiveIsHeld
+
+  if (dealCount < 3) {
+    cardResult1();
+
+    cardResult2();
+
+    cardResult3();
+
+    cardResult4();
+
+    cardResult5();
+
+    // Incr. dealCount
+
+    console.log(dealCount);
+  }
+});
+
+//// HOLD BTNs
+holdBtnOne.addEventListener('click', function (e) {
+  // Flip State Boolean
+  cardOneIsHeld ? (cardOneIsHeld = false) : (cardOneIsHeld = true);
+
+  // IF = Card IS HELD = ADD CSS Low B/Ness CLASS - ELSE Rem. it
+  cardOneIsHeld
+    ? cardOneDiv.classList.add('card-held')
+    : cardOneDiv.classList.remove('card-held');
+});
+
+holdBtnTwo.addEventListener('click', function (e) {
+  // Flip State Boolean
+  cardTwoIsHeld ? (cardTwoIsHeld = false) : (cardTwoIsHeld = true);
+
+  // IF = Card IS HELD = ADD CSS Low B/Ness CLASS - ELSE Rem. it
+  cardTwoIsHeld
+    ? cardTwoDiv.classList.add('card-held')
+    : cardTwoDiv.classList.remove('card-held');
+});
+
+holdBtnThree.addEventListener('click', function (e) {
+  // Flip State Boolean
+  cardThreeIsHeld ? (cardThreeIsHeld = false) : (cardThreeIsHeld = true);
+
+  // IF = Card IS HELD = ADD CSS Low B/Ness CLASS - ELSE Rem. it
+  cardThreeIsHeld
+    ? cardThreeDiv.classList.add('card-held')
+    : cardThreeDiv.classList.remove('card-held');
+});
+
+holdBtnFour.addEventListener('click', function (e) {
+  // Flip State Boolean
+  cardFourIsHeld ? (cardFourIsHeld = false) : (cardFourIsHeld = true);
+
+  // IF = Card IS HELD = ADD CSS Low B/Ness CLASS - ELSE Rem. it
+  cardFourIsHeld
+    ? cardFourDiv.classList.add('card-held')
+    : cardFourDiv.classList.remove('card-held');
+});
+
+holdBtnFive.addEventListener('click', function (e) {
+  // Flip State Boolean
+  cardFiveIsHeld ? (cardFiveIsHeld = false) : (cardFiveIsHeld = true);
+
+  // IF = Card IS HELD = ADD CSS Low B/Ness CLASS - ELSE Rem. it
+  cardFiveIsHeld
+    ? cardFiveDiv.classList.add('card-held')
+    : cardFiveDiv.classList.remove('card-held');
+});
